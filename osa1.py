f0a91dbda0a5e3184892da005ca4e1dd8c351e96
MustakallioJoonas <60339662+MustakallioJoonas@users.noreply.github.com>
2020-09-21 09:32:58 +0300

    ok

---
+++ osa1.py
@@ -0,0 +1,25 @@
+import random
+
+# Satunnaisuus
+print(random.randint(1, 10))
+
+# Nopan heittäminen
+import random
+print(random.randint(1, 6))
+
+# Kolikon heittäminen
+import random
+kolikko = ('kruuna', 'klaava')
+print (random.choice(kolikko))
+
+# Salasanan arpoja 
+import random
+
+
+
+# Sekoittaja 
+luvut = [1, 2, 3, 4, 5, 6, 7, 8]
+random.shuffle(luvut)
+print(luvut)
+
+# Vihollisen sijainnit
